40faf0813ea9e6f600c33a5e4b7508031f34ecf1
tefinirinasarobidy <fanomezantsoatefinirina@gmail.com>
2022-07-09 09:36:58 +0200

    laravel passport

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -29,9 +29,23 @@ class AuthController extends Controller
             $data['sexe'] = $request->sexe;
         }
         $user = Customers::create($data);
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+
+        $token->expires_at = $request->remember_me ? Carbon::now()->addWeeks(1) : Carbon::now()->addDay(1);
+        $token->save();
+        $access_token = $tokenResult->accessToken;
+        $token_type = 'Bearer';
+        $expires_at = Carbon::parse(
+            $tokenResult->token->expires_at
+        )->toDateTimeString();
+        
         return response()->json([
             'succes' => $user,
-            'message' => 'user enregistrer'
+            'message' => 'user enregistrer',
+            'access_token' => $access_token,
+            'token_type' => $token_type,
+            'expires_at' => $expires_at
         ]);
     }
     public function login(Request $request)
